63a1bdcc695bf07fe60db56a41c8a18d1cf05848
Kostis Kapelonis <kostis@codefresh.io>
2021-07-08 11:02:48 +0300

    Serve dashboard

--- app.go
+++ app.go
@@ -1,38 +1,28 @@
 package main
- 
+
 import (
-	"os"
 	"fmt"
 	"net/http"
-	// "github.com/golangci/golangci-lint/pkg/exitcodes"
+	"os"
 )
 
 func main() {
 
-	//Add a GPL3 package to cause havock 
+	//Add a GPL3 package to cause havock
 	// os.Setenv("test", string(exitcodes.Success))
 
-
 	c := os.Getenv("COLOR")
-	if len(c) == 0{
-		os.Setenv("COLOR", "#44B3C2") //Blue 44B3C2 and Yellow F1A94E  
-	}  
+	if len(c) == 0 {
+		os.Setenv("COLOR", "#44B3C2") //Blue 44B3C2 and Yellow F1A94E
+	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<html onclick=\"window.location.href = '/die'\" style='background:" + os.Getenv("COLOR") + "'> Requested: %s\n </html>", r.URL.Path)
+	http.HandleFunc("/callme", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<html style='background:"+os.Getenv("COLOR")+"'> Requested: %s\n </html>", r.URL.Path)
 	})
 
-	http.HandleFunc("/dashboard", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<html> DASHBOARD Requested: %s\n </html>", r.URL.Path)
-	})
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
 
-	http.HandleFunc("/die", func(w http.ResponseWriter, r *http.Request) {
-		die();
-	})
-
+	fmt.Println("Listening now at port 8080")
 	http.ListenAndServe(":8080", nil)
-}
-
-func die() {
-	os.Exit(3)
 }
